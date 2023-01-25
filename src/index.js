68bfa0e193eee42144c9c96d814664c254c4b3b1
juaniigauna <nachogauna83@gmail.com>
2023-01-25 11:46:30 -0300

    Changes

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
 const { Server } = require('socket.io')
 
-const server = new Server(process.env.PORT || 3000)
+const server = new Server(3000)
 
 const clients = {}
 
 server.on('connection', client => {
-    console.log("Cliente ", client.id, " conectado.")
-
     client.on('choose-name', name => {
         client.emit('name-result', { success: clients[name] === undefined })
         if (!clients[name]) {
             clients[name] = {}
-            console.log(name, 'Entró al chat.')
         }
+    })
 
-
+    client.on('change-name', name => {
+        delete clients[name]
     })
 
     client.on('send-message', data => {
